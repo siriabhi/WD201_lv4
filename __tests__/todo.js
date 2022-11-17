187d479e9f03d34d1f09cc3ccc4463f00a9c84b1
k-karthik7 <kkotha79@gmail.com>
2022-11-17 11:47:46 +0530

    changed

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,56 +1,50 @@
-const todoList = require('../todo');
-var d =new Date();
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+var d = new Date();
 
-
-const {all,markAsComplete, add,overdue,dueToday,dueLater,} =todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("todolist test suite", () => {
-    beforeAll(() => {
-        add(
-
-            {
-                title: "complete the work",
-                completed: false,
-                dueDate : new Date(new Date(d.setDate(d.getDate() - 1))).toLocaleDateString("en-CA")
-            },
-            {
-                title: "play basketball",
-                completed: false,
-                dueDate : new Date(new Date(d.setDate(d.getDate() + 1))).toLocaleDateString("en-CA")
-            }
-
-        );
-    })
-    test("should add new todo", () => {
-        const count =all.length;
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate : new Date().toLocaleDateString("en-CA")
-            }
-        );
-        expect(all.length).toBe(count + 1);
+  beforeAll(() => {
+    add(
+      {
+        title: "complete the work",
+        completed: false,
+        dueDate: new Date(
+          new Date(d.setDate(d.getDate() - 1))
+        ).toLocaleDateString("en-CA"),
+      },
+      {
+        title: "play basketball",
+        completed: false,
+        dueDate: new Date(
+          new Date(d.setDate(d.getDate() + 1))
+        ).toLocaleDateString("en-CA"),
+      }
+    );
+  });
+  test("should add new todo", () => {
+    const count = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
+    expect(all.length).toBe(count + 1);
+  });
 
-    test("Should mark todo as complete", () => {
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    });
-    test("checksing over dues", () => {
-
-        expect(overdue().length).toBe(1);
-    });
-    test("checking today's dues", () => {
-
-        expect(dueToday().length).toBe(1);
-    });
-    test("checking dues later", () => {
-
-        expect(dueLater().length).toEqual(1);
-    });
-    
-
-
-})
+  test("Should mark todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("checksing over dues", () => {
+    expect(overdue().length).toBe(1);
+  });
+  test("checking today's dues", () => {
+    expect(dueToday().length).toBe(1);
+  });
+  test("checking dues later", () => {
+    expect(dueLater().length).toEqual(1);
+  });
+});
